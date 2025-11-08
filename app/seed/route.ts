f1489162a3ab2f5fd98afbccbc78e5d4f53cfbb0
Marek Zelinka <mzelinka17@gmail.com>
2025-11-08 12:41:35 +0100

    database

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,8 +1,8 @@
-import bcrypt from 'bcrypt';
-import postgres from 'postgres';
-import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import bcrypt from "bcrypt";
+import postgres from "postgres";
+import { invoices, customers, revenue, users } from "../lib/placeholder-data";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -102,6 +102,10 @@ async function seedRevenue() {
 }
 
 export async function GET() {
+  return Response.json({
+    message:
+      "Uncomment this file and remove this line. You can delete this file when you are finished.",
+  });
   try {
     const result = await sql.begin((sql) => [
       seedUsers(),
@@ -110,7 +114,7 @@ export async function GET() {
       seedRevenue(),
     ]);
 
-    return Response.json({ message: 'Database seeded successfully' });
+    return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
